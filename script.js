312365a9f1d69f65bac3b3298691428da64eb05a
AfsalAfi <afsaldesktop@gmail.com>
2024-07-11 22:40:06 +0530

    --

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-
-// let slideNumber=1;
-// const images=2;
-
-//Make left scroll hide
-// const hide=()=>{
-    //     var button = document.getElementById('scroll-left');
-    //     button.style.display = 'none';
-    // }
-    
-    // const display=()=>{
-        //     var button = document.getElementById('scroll-left');
-        //     button.style.display = 'block';
-        // }
-        
-        
-        
-        // Slide Arrow funtions
-        // const nextSlide=()=>{
-            //     slider.style.transform=`translateX(-${slideNumber*310}px)`;
-            //     slideNumber++;
-            //     slideNumber===1 ?hide():display();
-            // }
-            
-// const prevSlide=()=>{
-    //     slider.style.transform=`translateX(-${(slideNumber-2)*310}px)`;
-    //     slideNumber--;
-    //     slideNumber===1 ?hide():display();
-    // }
-
-
-
     const leftbtn=document.querySelector('.scroll-left');
     const rightbtn=document.querySelector('.scroll-right');
     const slider=document.querySelector('.scrolling-wrapper');
@@ -48,5 +16,4 @@
         document.getElementById('scroll-right').style.display="block"   
         slider.scroll(0,0);
     })
-    
     
